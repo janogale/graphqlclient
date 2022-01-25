f1465857735f33e2a694abab21df465ea93c446b
janagale <mukhtar.mahomed@gmail.com>
2022-01-25 15:33:13 +0300

    Update graph url

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://catspaceship.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
